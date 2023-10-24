b987b6c5ba4106004f01ec428a93febeac1f175a
FelixThienel <reba.felix@web.de>
2023-10-24 23:08:16 +0200

    Aufgaben

--- src/org/hbrs/se1/ws23/uebung2/ConcreteMember.java
+++ src/org/hbrs/se1/ws23/uebung2/ConcreteMember.java
@@ -1,6 +1,6 @@
 package org.hbrs.se1.ws23.uebung2;
 
-public class ConcreteMember implements Member{
+public class ConcreteMember implements Member {
     int id;
 
     public ConcreteMember(int id) {
@@ -9,5 +9,10 @@ public class ConcreteMember implements Member{
 
     public Integer getID() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Member (ID = " + id +")";
     }
 }

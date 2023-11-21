59bd03ce1e3a2c53ef7a07b541118494ada7a5a8
Felix Thienel <reba.felix@web.de>
2023-11-21 14:55:59 +0100

    Übung 4 Aufgabe 1 a

---
+++ Übung4/Projekt Coll.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1700573191395" ID="ID_1976571638" MODIFIED="1700574174362" TEXT="Projekt Coll">
+<node CREATED="1700573427402" ID="ID_1727250166" MODIFIED="1700573640871" POSITION="right" TEXT="Unternehmen k&#xf6;nnen sich anmelden">
+<node CREATED="1700573455035" ID="ID_450663276" MODIFIED="1700573490218" TEXT="Unternehmen k&#xf6;nnen einzelne Themengebiete angeben "/>
+<node CREATED="1700573511758" ID="ID_160243208" MODIFIED="1700573596435" TEXT="Unternehmen k&#xf6;nnen Vorkenntnisse fordern "/>
+</node>
+<node CREATED="1700573618382" ID="ID_1392561886" MODIFIED="1700573697888" POSITION="left" TEXT="Studenten k&#xf6;nnen sich anmelden">
+<node CREATED="1700573652245" ID="ID_408751692" MODIFIED="1700573684576" TEXT="Studierende k&#xf6;nnen Unternehmen folgen um Benachrichtigungen zu offenen Stellen zu bekommen"/>
+<node CREATED="1700573698835" ID="ID_774156541" MODIFIED="1700573718463" TEXT="Studierende k&#xf6;nnen Themengebieten folgen um offene Stellen zu bekommen"/>
+</node>
+<node CREATED="1700573780842" ID="ID_1068551532" MODIFIED="1700573808102" POSITION="right" TEXT="Dauerhafte Stellenangebote von gro&#xdf;en Unternehmen">
+<node CREATED="1700573814276" ID="ID_40278444" MODIFIED="1700573845940" TEXT="Professoren k&#xf6;nnten die Arbeit als Extrapunkte in die Note zu passenden Modulen einflie&#xdf;en lassen"/>
+<node CREATED="1700573872074" ID="ID_1050393266" MODIFIED="1700573898225" TEXT="Professoren k&#xf6;nnten diese Stellen in ihren Modulen bewerben f&#xfc;r Praxiserfahrung"/>
+<node CREATED="1700574063171" ID="ID_926439954" MODIFIED="1700574087047" TEXT="Ein Praktikum k&#xf6;nnte als eigenes Praxismodul gewertet werden"/>
+</node>
+<node CREATED="1700573947464" ID="ID_659151409" MODIFIED="1700573964547" POSITION="left" TEXT="Bekannte Informationen">
+<node CREATED="1700573961201" ID="ID_939923341" MODIFIED="1700573992469" TEXT="Unternehmen sehen bereits absolvierte Module der bewerbenden Studenten "/>
+<node CREATED="1700574116140" ID="ID_1594683308" MODIFIED="1700574154462" TEXT="Studenten k&#xf6;nnen Bewertungen, Empfehlungen von anderen Studenten/Professoren einsehen"/>
+</node>
+<node CREATED="1700574177165" ID="ID_1322641407" MODIFIED="1700574196718" POSITION="right" TEXT="Arbeitsleben">
+<node CREATED="1700574198928" ID="ID_1256698017" MODIFIED="1700574213738" TEXT="Studenten haben einen Praktikumsnachweis f&#xfc;r nach dem Studium"/>
+<node CREATED="1700574218736" ID="ID_1085107941" MODIFIED="1700574245478" TEXT="Unternehmen k&#xf6;nnen f&#xfc;r sich geeignete Studenten direkt nach dem Studium anwerben"/>
+</node>
+</node>
+</map>
